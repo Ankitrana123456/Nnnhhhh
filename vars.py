a3c0895188f570b6be1aaa1b01f6e5f27e6e49ce
Ankitrana123456 <ankitrana73jio@gmail.com>
2025-07-15 23:57:57 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,17 +3,17 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "25933223"))
-API_HASH = environ.get("API_HASH", "6ef5a426d85b7f01562a41e6416791d3")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-OWNER = int(environ.get("OWNER", "7651551487"))
-CREDIT = environ.get("CREDIT", "𓍯𝙎𝙪𝙟𝙖𝙡⚝")
+OWNER = int(environ.get("OWNER", "5302639580"))
+CREDIT = environ.get("CREDIT", "𓍯ANKIT⚝")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7651551487').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '5302639580').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7651551487').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '5302639580').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
